9b7a4b2e76970814c57a2fbe9484ee5cba663f7e
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-11-07 18:03:18 +0100

    a test to do

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ chown -R "unmanic":"unmanic" /config
 
 FILE=""
 
-for base in /usr/local/lib /usr/lib; do
+for base in "/usr/local/lib" "/usr/lib"; do
   PY_DIR="$(ls -d "$base" 2>/dev/null | grep -E 'python[0-9]+\.[0-9]+' | sort -V | tail -n1)"
   if [ -n "${PY_DIR:-}" ] && [ -f "$PY_DIR/dist-packages/unmanic/libs/session.py" ]; then
     FILE="$PY_DIR/dist-packages/unmanic/libs/session.py"
@@ -27,13 +27,13 @@ sed -i -E '
 }
 ' "$FILE"
 
-sed -i -E '
-/^[[:space:]]*def[[:space:]]+register_unmanic[[:space:]]*\([[:space:]]*self[[:space:]]*,[[:space:]]*force[[:space:]]*=[[:space:]]*False[[:space:]]*\)[[:space:]]*:/,/^[^[:space:]]/{
-  /^[[:space:]]*("""|'"'"'""")[[:space:]]*$/{
-    a\        return True
-    q
-  }
-}
-' "$FILE"
+#sed -i -E '
+#/^[[:space:]]*def[[:space:]]+register_unmanic[[:space:]]*\([[:space:]]*self[[:space:]]*,[[:space:]]*force[[:space:]]*=[[:space:]]*False[[:space:]]*\)[[:space:]]*:/,/^[^[:space:]]/{
+#  /^[[:space:]]*("""|'"'"'""")[[:space:]]*$/{
+#    a\        return True
+#    q
+#  }
+#}
+#' "$FILE"
 
 exec gosu "unmanic" "/run.sh" "$@"
